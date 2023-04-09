553aa405689dc01ee0a4e1e4535fefd47541a07d
Hicham <hicham.kachrida@gmail.com>
2023-04-09 10:24:09 +0200

    Restart

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
 
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../templates');
+
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__.'/../var/cache',
+]);
+
+$template = $twig->load('index/index.twig.html');
+
+echo $template->render([
+    'title' => 'Page tihhhhtle',
+    'h1' => 'Page h2'
+]);
 
 ?>
